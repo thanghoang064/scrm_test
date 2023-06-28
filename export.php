9e3b061c2181957b0f21bbc2661f6159ed7a5daf
dinhtv7 <nguyenduyquangvinh2906@gmail.com>
2023-06-28 15:17:21 +0700

    vinh fix export lead

--- export.php
+++ export.php
@@ -87,13 +87,26 @@ if (!empty($_REQUEST['members'])) {
     $filename .= '_' . 'members';
 }
 if ($_REQUEST['module'] === "Leads"):
-    $rows = explode(PHP_EOL, $transContent);
+//    $transContent = chr(255) . chr(254) . mb_convert_encoding($transContent, 'UTF-16LE', 'UTF-8');
+//    $rows = explode(PHP_EOL, $transContent);
+//    $rows = mb_split(PHP_EOL, $transContent);
+    $rows = str_getcsv($transContent, PHP_EOL);
     $data = [];
     foreach ($rows as $row) {
-        $data[] = str_getcsv($row, ',', '"');
+        $dataChild = str_getcsv($row, ',', '"', "\\");
+        $dataChild2 = [];
+        foreach ($dataChild as $item) {
+            $dataChild2[] = stripcslashes(str_replace(["\"", "|"], ['', " "], $item));
+        }
+//        $data[] = str_getcsv($row, ',', '"');
+        $data[] = $dataChild2;
     }
 
     $xlsx = Shuchkin\SimpleXLSXGen::fromArray($data);
+//    echo '<pre>';
+//    print_r($xlsx);
+//    echo '</pre>';
+//    die;
     $xlsx->downloadAs("{$filename}.xlsx");
 else:
 
